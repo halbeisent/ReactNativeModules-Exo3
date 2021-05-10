4fb75aad55c65106bb3b17e0d5851be45c052063
halbeisent <thibault.halbeisendev@gmail.com>
2021-05-10 16:56:27 +0200

    L'exo 3 est bien avancé

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
 export default function App() {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1, alignItems: "center", flexDirection: "row"}}>
+      <Image style={{flex: 3, height: windowHeight, width: windowWidth}} source={require('./assets/rover.jpg')}></Image>
+      <Text style={{flex: 1, flexWrap: "wrap"}}>Le rover opportunity</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +16,5 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
